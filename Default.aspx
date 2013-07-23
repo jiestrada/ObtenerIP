8dbf03c74e70ee7fb462c60910eaa578ba807fff
José I. Estrada <jiestrada@live.com.mx>
2013-07-23 10:12:13 -0500

    Get IP
    Obtener IP con ASP.NET

---
+++ Default.aspx
@@ -0,0 +1,20 @@
+﻿<%@ Page Language="VB" AutoEventWireup="false" CodeFile="Default.aspx.vb" Inherits="ObtenerIP_Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title>Obtener dirección IP y Host Name</title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div style="font-weight:bold; border:1px;">
+        Tu dirección IP es: <asp:Label ID="lblIPAddress" runat="server" Text=""></asp:Label><br /><br />
+        Tu computadora/Host Name es: <asp:Label ID="lblHostName" runat="server" Text=""></asp:Label>
+        <br /><br />
+        Tu dirección IP detrás del Proxy es:- <asp:Label ID="lblIPBehindProxy" runat="server" Text=""></asp:Label>
+    </div>
+    </form>
+</body>
+</html>
---
+++ Default.aspx.vb
@@ -0,0 +1,22 @@
+﻿Imports System.Net
+
+Partial Class ObtenerIP_Default
+    Inherits System.Web.UI.Page
+
+    Protected Sub Page_Load(sender As Object, e As EventArgs) Handles Me.Load
+        Dim strHostName As String = Dns.GetHostName()
+        Dim ipEntry As IPHostEntry = Dns.GetHostEntry(strHostName)
+
+        lblIPAddress.Text = Convert.ToString(ipEntry.AddressList(ipEntry.AddressList.Length - 1))
+        lblHostName.Text = Convert.ToString(ipEntry.HostName)
+
+        'Find IP Address Behind Proxy Or Client Machine In ASP.NET
+        Dim IPAdd As String = String.Empty
+        IPAdd = Request.ServerVariables("HTTP_X_FORWARDED_FOR")
+
+        If String.IsNullOrEmpty(IPAdd) Then
+            IPAdd = Request.ServerVariables("REMOTE_ADDR")
+            lblIPBehindProxy.Text = IPAdd
+        End If
+    End Sub
+End Class
